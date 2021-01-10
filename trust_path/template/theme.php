50511c02d90868bfbb24f38b4d4a32c45051b1d1
孙轶伟 <sunyiwei.24601@bytedance.com>
2021-01-11 02:51:44 +0800

    隐藏

--- trust_path/template/theme.php
+++ trust_path/template/theme.php
@@ -5,18 +5,16 @@
 <meta name="viewport" content="width = 620" />
 <title><?php e(t(DURA_TITLE)) ?> | <?php e(t(DURA_SUBTITLE)) ?></title>
 <link href="<?php echo DURA_URL; ?>/css/style.css" rel="stylesheet" type="text/css" media="screen" />
-<script type="text/javascript" src="http://www.google.com/jsapi"></script>
-<script type="text/javascript"><!--
-google.load("language", "1");
-duraUrl = "<?php e(DURA_URL) ?>";
-GlobalMessageMaxLength = <?php e(DURA_MESSAGE_MAX_LENGTH) ?>;
-useComet = <?php e(DURA_USE_COMET) ?>;
-//-->
-</script>
 <script type="text/javascript" src="<?php e(DURA_URL) ?>/js/jsapi.js"></script>
 <script type="text/javascript" src="<?php e(DURA_URL) ?>/js/jquery.min.js"></script>
 <script type="text/javascript" src="<?php e(DURA_URL) ?>/js/jquery-ui.min.js"></script>
-
+    <script type="text/javascript"><!--
+        google.load("language", "1");
+        duraUrl = "<?php e(DURA_URL) ?>";
+        GlobalMessageMaxLength = <?php e(DURA_MESSAGE_MAX_LENGTH) ?>;
+        useComet = <?php e(DURA_USE_COMET) ?>;
+        //-->
+    </script>
     <script type="text/javascript" src="<?php e(DURA_URL) ?>/js/translator.js"></script>
 <script type="text/javascript" src="<?php e(DURA_URL) ?>/js/language/<?php e(Dura::$language) ?>.js"></script>
 <script type="text/javascript" src="<?php e(DURA_URL) ?>/js/snowStorm.js"></script>
